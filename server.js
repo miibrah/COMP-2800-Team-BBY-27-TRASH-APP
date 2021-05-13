6e7d21fbf90b25c10666e877810948b92ecd66d3
Samuel Cheon <samuelcalling@gmail.com>
2021-05-13 13:43:47 -0700

    adjusted server.js to serve up landing.html

--- server.js
+++ server.js
@@ -3,6 +3,29 @@ const app = express()
 const connectDB = require ('./DB/connection');
 const routes = require('./API/routes') // includes the routes.js file
 
+const fs = require("fs");
+
+// APP GETS
+app.get('/', function (req, res) {
+
+    // just being silly but you can change the header response so that it
+    // doesn't say Node.js, but some custom info about your app
+    res.set('Server', 'Wazubi Engine');
+    res.set('X-Powered-By', 'Magical Pixies');
+
+    fs.readFile("./landing.html", function (error, pgRes) {
+        if (error) {
+            res.writeHead(404);
+            res.write(msg404);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(pgRes);
+        }
+
+        res.end();
+    });
+
+});
 
 
 
