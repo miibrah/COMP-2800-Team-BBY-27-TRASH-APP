fd6311bca16187fd3f9913fe27875e679ab20a82
Samuel Cheon <samuelcalling@gmail.com>
2021-05-19 23:47:38 -0700

    updated and created login + register pages [front end and back end]

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const User = require('./model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'asdinahosidha;sidh!@##$^#$%#asluidalisudhlauisdhasuidh';
+
+// mongoose.connect();
+mongoose.connect('mongodb+srv://dbUser:dbUser@cluster0.olikl.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+    useCreateIndex: true
+});
 const connectDB = require ('./DB/connection');
 const routes = require('./API/routes') // includes the routes.js file
 
@@ -11,6 +24,8 @@ const Port = process.env.PORT || 3000;
 app.use('/css', express.static('./private/css'));
 app.use('/images', express.static('./private/images'));
 app.use('/js', express.static('./private/js'));
+app.use(bodyParser.json())
+
 app.set('views', express.static('./views'));
 app.set('view engine', 'ejs');
 
@@ -138,8 +153,103 @@ app.get('/quiz.html', function(req, res){
 });
 
 
+app.get('/login.html', function(req, res){
+
+    fs.readFile("./login.html", function (error, pgRes) {
+        if (error) {
+            res.writeHead(404);
+            res.write(msg404);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(pgRes);
+        }
+
+        res.end();
+    });
+});
+
+app.get('/register.html', function(req, res){
+
+    fs.readFile("./register.html", function (error, pgRes) {
+        if (error) {
+            res.writeHead(404);
+            res.write(msg404);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(pgRes);
+        }
+
+        res.end();
+    });
+});
 
 
+app.post('/api/register', async (req, res) => {
+    console.log(req.body);
+    
+    const { username, password: plainTextPassword } = req.body;
+
+    if(!username || typeof username !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid username' })
+    }
+
+    if(!plainTextPassword || typeof plainTextPassword !== 'string') {
+        return res.json({ status: 'error', error: 'Invalid password' })
+    }
+
+    if(plainTextPassword.length < 5) {
+        return res.json({ status: 'error', error: 'Password too small. Should be at least 6 characters.' })
+    }
+
+    const password = await bcrypt.hash(plainTextPassword, 10);
+
+    // console.log(await bcrypt.hash(password, 10));
+    console.log(password);
+
+
+    try {
+        const response = await User.create({
+            username,
+            password
+        })
+        console.log("User created succesfully", response)
+    } catch(error) {
+        if (error.code === 11000) {
+            return res.json({ status: 'error', error: 'Username already in use' })
+        }
+        throw error;
+        // console.log(JSON.stringify(error));
+        // return res.json({ status: 'error' })
+    }
+
+    res.json({ status: 'ok' })
+})
+
+app.post('/api/login', async (req, res) => {
+
+    const { username, password } = req.body;
+    const user = await User.findOne({ username }).lean();
+
+    if(!user) {
+        return res.json({ status: 'error', error: 'Invalid username/password' });
+    }
+
+    if( await bcrypt.compare(password, user.password)) {
+        // the username , password combination is succesful
+
+        const token = jwt.sign(
+            {   id: user._id, 
+                username: user.username 
+            }, 
+            JWT_SECRET
+            )
+
+        return res.json({ status: 'ok', data: token });
+    }
+
+    res.json( {status: 'error', error: 'Invalid username/password' } )
+
+})
 
 app.use(express.json()) 
 app.use(routes) 
