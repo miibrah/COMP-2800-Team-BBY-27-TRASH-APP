c5b895541a87de7a238f4fa0fb71bae57b0ee4f3
DESKTOP-LVIDGEJ\권오현1 <ohhyun00@gmail.com>
2021-05-27 13:16:33 -0700

    made questions

--- DB/Question.js
+++ DB/Question.js
@@ -41,10 +41,10 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
         },
 
         {
-            "text": " pizza deliverybox"
+            "text": " Pizza deliverybox"
         },
         {
-            "text": "paper napkins"
+            "text": "Paper napkins"
         }
 
       ]
@@ -53,18 +53,18 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
     },
     {
       "category": "Trash",
-      "correct": "Milk carton",
-      "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
+      "correct": "Trash Cart",
+      "questionss": "Strofoam goes in which cart?",
       "difficulty" : "easy",    
     "incorrect_answer": [
-        { "text": "Apple-core"
+        { "text": "Recycling cart"
       },
 
       {
-          "text": " pizza deliverybox"
+          "text": "Shopping cart"
       },
       {
-          "text": "paper napkins"
+          "text": "In your car"
       }
 
     ]
@@ -73,18 +73,18 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
   },
   {
     "category": "Trash",
-    "correct": "Milk carton",
-    "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
+    "correct": "Trash Cart",
+    "questionss": "Plastic hangers go in which cart?",
     "difficulty" : "easy",    
   "incorrect_answer": [
-      { "text": "Apple-core"
+      { "text": "Recycling cart"
     },
 
     {
-        "text": " pizza deliverybox"
+        "text": " Shopping cart"
     },
     {
-        "text": "paper napkins"
+        "text": "In your car"
     }
 
   ]
@@ -93,18 +93,18 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
 },
 {
   "category": "Trash",
-  "correct": "Milk carton",
-  "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
+  "correct": "Trash Cart",
+  "questionss": "Shampoo and lotion bottles go in which cart?",
   "difficulty" : "easy",    
 "incorrect_answer": [
-    { "text": "Apple-core"
+    { "text": "Recycling cart"
   },
 
   {
-      "text": " pizza deliverybox"
+      "text": " Shopping cart"
   },
   {
-      "text": "paper napkins"
+      "text": "In your car"
   }
 
 ]
@@ -113,18 +113,18 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
 },
 {
   "category": "Trash",
-  "correct": "Milk carton",
-  "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
+  "correct": "Trash",
+  "questionss": "Where would you place  coffee cup?",
   "difficulty" : "easy",    
 "incorrect_answer": [
-    { "text": "Apple-core"
+    { "text": "Recycle"
   },
 
   {
-      "text": " pizza deliverybox"
+      "text": " Compost"
   },
   {
-      "text": "paper napkins"
+      "text": "Fridge"
   }
 
 ]
@@ -133,18 +133,78 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
 },
 {
   "category": "Trash",
-  "correct": "Milk carton",
-  "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
+  "correct": "Trash",
+  "questionss": "Where would you place  plastic straw?",
   "difficulty" : "easy",    
 "incorrect_answer": [
-    { "text": "Apple-core"
+    { "text": "Recycle"
   },
 
   {
-      "text": " pizza deliverybox"
+      "text": " Compost"
   },
   {
-      "text": "paper napkins"
+      "text": "Back of your car"
+  }
+
+]
+
+
+},
+{
+  "category": "Recyle",
+  "correct": "Recyle",
+  "questionss": "Where would you place  yogurt container?",
+  "difficulty" : "easy",    
+"incorrect_answer": [
+    { "text": "Compost"
+  },
+
+  {
+      "text": " Trash"
+  },
+  {
+      "text": "In your room"
+  }
+
+]
+
+
+},
+{
+  "category": "Recycle",
+  "correct": "True",
+  "questionss": "Plastic bags are not accepted in the recycling bin",
+  "difficulty" : "easy",    
+"incorrect_answer": [
+    { "text": "False"
+  },
+
+  {
+      "text": "Maybe"
+  },
+  {
+      "text": "None of above"
+  }
+
+]
+
+
+},
+{
+  "category": "Recycling",
+  "correct": "Recycling Cart",
+  "questionss": "Cartons go in which cart?",
+  "difficulty" : "easy",    
+"incorrect_answer": [
+    { "text": "Trash cart"
+  },
+
+  {
+      "text": "Shopping cart"
+  },
+  {
+      "text": "Your room cart"
   }
 
 ]
@@ -153,81 +213,21 @@ const QuestionSchemas = mongoose.model('Question', QuestionSchema)
 },
 {
   "category": "Trash",
-  "correct": "Milk carton",
-  "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
+  "correct": "Trash Cart",
+  "questionss": "Plastic buckets go in which cart?",
   "difficulty" : "easy",    
 "incorrect_answer": [
-    { "text": "Apple-core"
+    { "text": "Recycling cart"
   },
 
   {
-      "text": " pizza deliverybox"
+      "text": " Shopping cart"
   },
   {
-      "text": "paper napkins"
+      "text": "In your car"
   }
 
 ]
-
-
-},
-{
-  "category": "Trash",
-  "correct": "Milk carton",
-  "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
-  "difficulty" : "easy",    
-"incorrect_answer": [
-    { "text": "Apple-core"
-  },
-
-  {
-      "text": " pizza deliverybox"
-  },
-  {
-      "text": "paper napkins"
-  }
-
-]
-
-
-},
-{
-  "category": "Trash",
-  "correct": "Milk carton",
-  "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
-  "difficulty" : "easy",    
-"incorrect_answer": [
-    { "text": "Apple-core"
-  },
-
-  {
-      "text": " pizza deliverybox"
-  },
-  {
-      "text": "paper napkins"
-  }
-
-]
-
-
-},
-    {
-        "category": "Trash",
-        "correct": "Milk carton",
-        "questionss": "Which of the following can NOT be placed in a school composting container or in your yard waste cart at home?",
-        "difficulty" : "easy",    
-      "incorrect_answer": [
-          { "text": "Apple-core"
-        },
-
-        {
-            "text": " pizza deliverybox"
-        },
-        {
-            "text": "paper napkins"
-        }
-
-      ]
 
       
     }
--- private/js/end.js
+++ private/js/end.js
@@ -13,8 +13,9 @@ username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
 });
 
+
 saveHighScore = (e) => {
-    e.preventDefault();
+     e.preventDefault();
 
     let username = "anonymous";
     $.ajax({
@@ -41,13 +42,14 @@ saveHighScore = (e) => {
                     }
                 });
 
-    // const score = {
-    //     score: mostRecentScore,
-    //     name: username.value,
-    // };
-    // highScores.push(score);
-    // highScores.sort((a, b) => b.score - a.score);
-    // highScores.splice(5);
+
+// const score = {
+//     score: mostRecentScore,
+//     name: username.value,
+// };
+// highScores.push(score);
+// highScores.sort((a, b) => b.score - a.score);
+// highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
     window.location.assign('/landing.html');
